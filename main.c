6e9ace75ac08beea2ebb69ed4f0b9f618354570f
hareru03 <hareru03@sookmyung.ac.kr>
2022-10-11 17:33:44 +0900

    05

--- main.c
+++ main.c
@@ -3,19 +3,38 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a) {
+int factorial(int a) { 
 	
-	return a*a;
+	int res=1;
+	int i;
+	for(i=1;i<=a;i++)
+		res=res*i;
+	
+	return res; 
 }
 
+int combination(int a, int b) { 
+	
+	int x,y,z,res;
+	
+	x=factorial(a);
+	y=factorial(b);
+	z=factorial(a-b);
+
+	res=x/(y*z);
+	
+	return res;
+}
 
 int main(int argc, char *argv[]) {
 	
-	int a=2; 
+	int a,b,x;
 	
-	a=square(a); 
+	printf("정수 2개를 입력하시오 :");
+	scanf("%d %d",&a,&b); 
+	x=combination(a,b);
+	printf("%d",x); // 값이 전부 0으로 나옴 
 	
-	printf("a=%i\n",a);
-
-} 
+	return 0;
+}
 
